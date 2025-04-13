aa69b3e499f9c2a48079f875f73a8f91dd01747a
JSpanjaard07 <157476395+JSpanjaard07@users.noreply.github.com>
2025-04-13 13:00:59 +0200

    final 13-4
    Docker Compose werkt niet, ik weet niet waarom, ik kan dus niet verder werken voor Maandag

--- html/index.php
+++ html/index.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SushiDaddy</title>
+    <link rel="stylesheet" href="css\main.css">
+    <link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet'>
 </head>
 <body>
     <header>
