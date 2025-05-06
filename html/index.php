35b26a22822a890762f90907df445884e58a1ee1
JSpanjaard07 <157476395+JSpanjaard07@users.noreply.github.com>
2025-05-06 22:57:38 +0200

    Crud
    Ik heb CRUD toegevoegd en een zoek functie maar voor 1 of andere reden werkt ineens heel mijn CSS niet meer, ik ben er nu al 4 uur mee bezig geweest maar ik kan helaas gewoon niet verder

--- html/index.php
+++ html/index.php
@@ -30,6 +30,62 @@
             </div>
         </nav>
     </header>
-    
+    <div class="bigBody">
+        <main>
+
+            <?php
+
+            require_once ("php/db.php");
+
+            $db = new db();
+
+            $conn = $db->get_connection();
+
+            $result = [];
+
+            $sql = "SELECT * FROM menu";
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $search = "%" . $_POST["search"] . "%";
+            $sql = "SELECT * FROM menu WHERE name LIKE :search";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(['search' => $search]);
+            $result = $stmt->fetchAll();
+            }
+
+
+            $template = '
+            <div class="gerecht-item space-between column">
+            <div>
+            <h1 class="gerecht-naam">%s</h1>
+            <h2 class="gerecht-beschrijving">%s</h2>
+            </div>
+            <h2 class="gerecht-prijs">€%s</h2>
+            </div>
+            ';
+
+            ?>
+
+            <form method="POST" action="index.php" class="zoekbox column">
+                <input type="text" name="search" class="filteren">
+                <button type="submit" class="zoeken">Zoeken</button>
+            </form>
+
+            <section class="generale-spacer row">
+                <?php
+                foreach ($result as $row) {
+                echo sprintf($template, $row["name"], $row["description"], $row["price"]);
+                }
+                ?>
+            </section>
+
+        </main>
+        <div class="order">
+            <p>waaaa</p>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
 </body>
 </html>
--- html/php/add_menu.php
+++ html/php/add_menu.php
@@ -26,6 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         ":price" => $price,
         ":description" => $description
     ]);
-
+    
 
 }
+header("../admin/index.php");
